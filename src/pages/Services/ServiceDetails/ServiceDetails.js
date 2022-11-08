d44fcf0cc387a830ba8357788c5f1db69f5e3702
NahianNahin <nahian1927040@stud.kuet.ac.bd>
2022-11-09 01:29:43 +0600

    service detail complete

--- src/pages/Services/ServiceDetails/ServiceDetails.js
+++ src/pages/Services/ServiceDetails/ServiceDetails.js
@@ -5,10 +5,17 @@ const ServiceDetails = () => {
     const details = useLoaderData();
     const { _id, title, img, service_charge, description } = details;
     return (
-        <div>
-            <h1 className='text-center text-3xl my-10 '>Service Details</h1>
-            <div>
-                
+        <div className='mb-52 shadow-lg border-t-2 border-primary p-5 mt-4 mx-2'>
+            <h1 className='text-center text-4xl my-10 '>Service Details</h1>
+            <div className='flex flex-col-reverse lg:flex-row justify-evenly items-center lg:gap-5'>
+                <div className='lg:w-2/3 p-5'>
+                    <p className='text-left text-3xl font-semibold p-5'>{title}</p>
+                    <p className='text-left  '>{description}</p>
+                    <p className='text-left md:text-xl '>I provide my service at a very low cost--- <span className='text-primary lg:text-2xl font-semibold'>${service_charge}</span></p>
+                </div>
+                <div className='lg:w-1/3 p-2'>
+                    <img className='rounded-lg' src={img} alt="" />
+                </div>
             </div>
 
         </div>
