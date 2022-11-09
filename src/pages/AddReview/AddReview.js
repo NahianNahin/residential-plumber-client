cebd4b21a6b52fc7a5e142bae123235991672966
NahianNahin <nahian1927040@stud.kuet.ac.bd>
2022-11-09 18:04:01 +0600

    add review Post api complete

--- src/pages/AddReview/AddReview.js
+++ src/pages/AddReview/AddReview.js
@@ -4,11 +4,49 @@ import { AuthContext } from '../../contexts/AuthProvider';
 
 const AddReview = () => {
     const service = useLoaderData();
+    const {_id,title,} = service;
     const { user } = useContext(AuthContext);
+
+    const handleAddReviews = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = user?.email || 'Unregistered'
+        const rating = form.rating.value;
+        const review = form.review.value;
+
+        const Review = {
+            service: _id,
+            serviceName: title,
+            User_name: user.displayName,
+            date: new Date(),
+            email,
+            review,
+            rating
+        }
+        console.log(Review);
+        fetch('http://localhost:5000/reviews', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(Review),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data);
+                if(data.acknowledged){
+                    form.reset();
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
     return (
         <div>
 
-            <form className='card shadow-lg p-10 mx-10 lg:mx-28 mt-16 mb-52 border-t-2 border-primary'>
+            <form onSubmit={handleAddReviews} className='card shadow-lg p-10 mx-10 lg:mx-28 mt-16 mb-52 border-t-2 border-primary'>
                 <h1 className='text-center text-3xl my-5 '>Add Your Review</h1>
                 <p className='text-center text-xl'>Give your valueable review for <span className='text-primary font-semibold'>{service.title}</span></p>
                 <div className="form-control">
